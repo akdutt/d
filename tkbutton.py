0a253bcd369ab55b6a940c49f9f837fde967b39c
Ankur <ankurkumar222@outlook.com>
2018-04-17 23:23:49 +0530

    2nd commit button added

--- tkbutton.py
+++ tkbutton.py
@@ -8,7 +8,16 @@ bottomFrame=Frame(root)
 bottomFrame.pack(side=BOTTOM)
 
 button1=Button(topFrame,text="Button 1",fg="green")
+button2=Button(topFrame,text="Button 2",fg="green")
+button3=Button(topFrame,text="Button 3",fg="green")
+button4=Button(bottomFrame,text="Button 4",fg="red")
+button5=Button(bottomFrame,text="Button 5",fg="red")
 
+button1.pack()
+button2.pack()
+button3.pack()
+button4.pack()
+button5.pack()
 
 
 
