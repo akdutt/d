91e3f35b56ecffee67943b2ead898ebec6df4b24
Ankur <ankurkumar222@outlook.com>
2018-04-17 23:10:00 +0530

    1st commit

---
+++ tk.py
@@ -0,0 +1,6 @@
+from Tkinter import *
+
+root = Tk()
+theLabel = Label(root, text="WELLCOME TO SPY CHAT APPLICATION")
+theLabel.pack()
+root.mainloop()
---
+++ tkbutton.py
@@ -0,0 +1,15 @@
+from Tkinter import *
+
+root = Tk()
+
+topFrame=Frame(root)
+topFrame.pack()
+bottomFrame=Frame(root)
+bottomFrame.pack(side=BOTTOM)
+
+button1=Button(topFrame,text="Button 1",fg="green")
+
+
+
+
+root.mainloop()
